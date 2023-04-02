d8ce4cd055e40809c4b9f472ab27dda03c6a1e91
seagull <3132177523@qq.com>
2023-04-02 19:25:12 +0800

    push commit1

--- Git_test01/src/main/java/com/atguigu/git/GitTest.java
+++ Git_test01/src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git");
         System.out.println("hello Git4");
+        System.out.println("push test!");
     }
 }
