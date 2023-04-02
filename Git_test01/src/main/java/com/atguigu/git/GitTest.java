d3b62b4c29793693002fcc723e7d0703fe16d0a0
FSRAAA <129602368+FSRAAA@users.noreply.github.com>
2023-04-02 19:29:36 +0800

    Update GitTest.java

--- Git_test01/src/main/java/com/atguigu/git/GitTest.java
+++ Git_test01/src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git4");
         System.out.println("push test!");
+        System.out.println("push2 test!");
     }
 }
